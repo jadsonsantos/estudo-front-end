42b79461084104521a2748abd6638b85a511150e
Jadson Santos <jadsondossantos5@gmail.com>
2019-12-14 20:42:44 -0300

    Slides in the side navigation from the left and covers the whole page

--- side-navigation/main.js
+++ side-navigation/main.js
@@ -1,13 +1,9 @@
 /* Set the width of the side navigation to 250px */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    document.getElementById("mySidenav").style.width = "100%";
 }
   
   /* Set the width of the side navigation to 0 */
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
-    document.body.style.backgroundColor = "white";
 }
